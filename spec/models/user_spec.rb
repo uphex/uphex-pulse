34a62bb1a048a47d19c3dbaae6c3124ce8d018f6
John Feminella <jxf@jxf.me>
2014-01-03 16:14:31 -0500

    Augments User model with password-strategy fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,15 @@ describe User do
   context "validations" do
     it { expect(subject).to validate_presence_of(:email) }
     it { expect(subject).to validate_presence_of(:password_hash) }
+    it { expect(subject).to validate_presence_of :password }
+    it { expect(subject).to validate_presence_of :password_confirmation }
+    it { expect(subject).to validate_confirmation_of :password }
+  end
+
+  context "#password_hash=" do
+    it "assigns to the password hash" do
+      expect(subject).to receive :password_hash=
+      subject.password = 'unencrypted-password'
+    end
   end
 end
