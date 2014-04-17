40ac1df8d2e7f9f96bbb781b2d338768e6e3c7be
John Feminella <jxf@jxf.me>
2014-04-17 00:47:13 -0400

    Supports getting app_constant for first mounted Padrino app

--- spec/support/padrino/environment.rb
+++ spec/support/padrino/environment.rb
@@ -2,3 +2,7 @@ def app(app = nil, &blk)
   @app ||= block_given? ? app.instance_eval(&blk) : app
   @app ||= Padrino.application
 end
+
+def app_class
+  Padrino.mounted_apps.first.app_constant
+end
