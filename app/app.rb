1e99624240d2e57710b2ae28dea5d3d96fc5b134
sashee <gsashee@gmail.com>
2014-04-28 19:01:52 +0200

    refactor

--- app/app.rb
+++ app/app.rb
@@ -7,12 +7,7 @@ module UpHex
 
     enable :sessions
     register UpHex::Initializers::Warden
-
-    after do
-      if request.url.end_with?('css')
-        headers "Content-Type" => "text/css;charset=utf-8"
-      end
-    end
+    register UpHex::Initializers::SinatraAssetpack
 
     ##
     # Caching support.
@@ -45,33 +40,7 @@ module UpHex
     # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
     #
 
-    require 'sinatra/assetpack'
-    register Sinatra::AssetPack
 
-    assets {
-      serve '/js',     from: Gem::Specification.find_by_name("uphex-flatty").gem_dir+'/assets/javascripts'
-      serve '/css',    from: Gem::Specification.find_by_name("uphex-flatty").gem_dir+'/assets/stylesheets'
-      serve '/fonts',    from: Gem::Specification.find_by_name("uphex-flatty").gem_dir+'/assets/fonts'
-      serve '/public/css',    from: File.dirname(__FILE__)+'/../public/scss'
-      serve '/public/js',    from: File.dirname(__FILE__)+'/../public/js'
-      js :app, '/js/app.js', [
-          '/js/jquery/jquery.min.js',
-          '/js/jquery/jquery-migrate.min.js',
-          '/js/bootstrap/bootstrap.min.js',
-          '/js/theme.js',
-          '/public/js/test.js'
-      ]
-
-      css :application, '/css/application.css', [
-          '/css/bootstrap/bootstrap.css',
-          '/css/light-theme.css',
-          '/css/theme-colors.css',
-          '/public/css/test.css'
-      ]
-
-      js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
-      css_compression :simple   # :simple | :sass | :yui | :sqwish
-    }
 
     ##
     # You can configure for a specified environment like:
