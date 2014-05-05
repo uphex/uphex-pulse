a570cc366c4d3329a9274a5311411b13e514e220
sashee <gsashee@gmail.com>
2014-05-05 19:20:26 +0200

    portfolios refactor

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,9 @@
 require 'active_record'
 
 class Portfolio < ActiveRecord::Base
+
+  validates :name,
+            :presence => true, uniqueness: true
+
   belongs_to :organization, :class_name => "Organization", :foreign_key=> "organizations_id"
 end
