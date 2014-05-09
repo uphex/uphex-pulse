4230959e6ef726fb9f3c0f94dfadac7d04e3f519
John Feminella <jxf@jxf.me>
2014-05-09 12:27:57 -0400

    Adds Organizations

---
+++ spec/models/organization_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'spec/support/templates/active_record_model'
+require 'app/models/organization'
+
+describe Organization do
+  context "validations" do
+    validation_spec_for :presence, :name
+    validation_spec_for :presence, :slug
+    validation_spec_for :uniqueness, :slug
+  end
+end
