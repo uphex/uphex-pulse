49ec88be9cc7d93ae665cf945a62fe1d8c17f789
John Feminella <jxf@jxf.me>
2014-05-11 16:00:38 -0400

    Adds Shoulda::Matchers::ActiveRecord to RSpec context

--- spec/support/base/shoulda_matchers.rb
+++ spec/support/base/shoulda_matchers.rb
@@ -1,5 +1,8 @@
 RSpec.configure do |config|
   require 'shoulda-matchers'
   require 'shoulda/matchers/active_model'
+  require 'shoulda/matchers/active_record'
+
   config.include Shoulda::Matchers::ActiveModel
+  config.include Shoulda::Matchers::ActiveRecord
 end
