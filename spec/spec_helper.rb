4d0b2d131d0edaccccb71fe1c9d38ac98506574a
John Feminella <jxf@jxf.me>
2014-05-12 00:11:36 -0400

    Removes spec/support autoloading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,3 @@
-# Require support files.
-Dir[File.join %w{spec support base ** *.rb}].each do |f|
-  require f
-end
-
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
