9de7d5fce5add60eb512b672a208d7dc86b3956a
John Feminella <jxf@jxf.me>
2014-05-12 00:10:00 -0400

    Adds ActiveRecord spec support helper

---
+++ spec/support/active_record.rb
@@ -0,0 +1,16 @@
+require 'active_record'
+require 'yaml'
+require 'erb'
+
+YAML.load(ERB.new(File.read File.join('.', 'config', 'database.yml')).result).each { |name, hash|
+  symbolized_hash = hash.each_with_object({}) do |(k, v), h|
+    h[k.to_sym] = v
+  end
+  ActiveRecord::Base.configurations[name.to_sym] = symbolized_hash
+}
+
+ActiveRecord::Base.establish_connection(
+  ActiveRecord::Base.configurations[(ENV['RACK_ENV'] || 'test').to_sym]
+)
+
+I18n.enforce_available_locales = true
