3f1ecd131150716e2f6affaf8d11c524d54f5547
John Feminella <jxf@jxf.me>
2014-05-12 01:19:42 -0400

    Adds hstore migration for fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "organization_memberships", id: false, force: true do |t|
     t.integer  "organization_id", null: false
