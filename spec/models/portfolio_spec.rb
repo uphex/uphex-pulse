5dd4b7b0da5dad21e661c328eedef9a61ee38283
John Feminella <jxf@jxf.me>
2014-05-11 23:48:21 -0400

    Adds Streams

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -7,4 +7,8 @@ describe Portfolio do
     validation_spec_for :presence, :organization
     validation_spec_for :presence, :name
   end
+
+  context "associations" do
+    association_spec_for :have_many, :streams
+  end
 end
