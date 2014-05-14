621cd183eb4af5c4090d948b0df93b7be1ac9f94
John Feminella <jxf@jxf.me>
2014-05-14 01:48:07 -0400

    Standardizes navigation partials, adds specs

--- spec/requests/navigation_spec.rb
+++ spec/requests/navigation_spec.rb
@@ -2,9 +2,18 @@ require 'environment_spec_helper'
 
 describe "page navigation request" do
   context "when not logged in" do
-    it "contains sign in link" do
+    it "contains sign in and sign up links" do
       get '/'
       expect(last_response.body).to include "sign up"
+      expect(last_response.body).to include "sign in"
+    end
+  end
+
+  context "when logged in" do
+    it "contains user name links" do
+      app_class.any_instance.stub :current_user => User.new(:name => 'Bobby Tables')
+      get '/'
+      expect(last_response.body).to include 'Bobby Tables'
     end
   end
 end
