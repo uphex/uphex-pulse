7424645ebf3c3ebd6a4c6e656c72dc0864fdb80e
John Feminella <jxf@jxf.me>
2014-05-16 04:21:44 -0400

    Adds Compass.sass_engine_options into app for Sass rendering
    Setting the :scss and :sass options gives Padrino a source for how to
    process these files from Compass.

--- config/initializers/compass_assets.rb
+++ config/initializers/compass_assets.rb
@@ -4,10 +4,16 @@ module UpHex
       def self.registered(app)
         require 'sass/plugin/rack'
 
+        app.use Sass::Plugin::Rack
+        Sass::Plugin.on_updating_stylesheet do |t_in, t_out|
+          app.logger.debug "compiling:\nin: #{t_in}\nout: #{t_out}"
+        end
+
         Compass.configuration do |config|
           config.project_path         = Padrino.root
           config.project_type         = :stand_alone
           config.output_style         = :compressed
+          config.preferred_syntax     = :scss
 
           config.http_path            = "/"
           config.sass_dir             = "app/assets/stylesheets"
@@ -15,6 +21,8 @@ module UpHex
           config.http_images_path     = "/images"
           config.css_dir              = "public/stylesheets"
           config.javascripts_dir      = "public/javascripts"
+
+          config.add_import_path 'app/assets/stylesheets'
 
           config.on_stylesheet_error do |f, m|
             puts "---- error in #{f}"
@@ -25,7 +33,9 @@ module UpHex
         Compass.configure_sass_plugin!
         Compass.handle_configuration_change!
 
-        app.use Sass::Plugin::Rack
+        app.logger.devel "Initializing Compass with options: #{Compass.sass_engine_options}"
+        app.set :sass, Compass.sass_engine_options
+        app.set :scss, Compass.sass_engine_options
       end
     end
   end
