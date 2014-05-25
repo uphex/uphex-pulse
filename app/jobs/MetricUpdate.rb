d030d54acb27faca69214a4dc23f2377341f48ee
sashee <gsashee@gmail.com>
2014-05-25 23:47:29 +0200

    twitter and google analytics integration

--- app/jobs/MetricUpdate.rb
+++ app/jobs/MetricUpdate.rb
@@ -3,26 +3,60 @@ class MetricUpdate
   def self.perform(metric_id)
     puts 'MetricUpdate invoked'
 
-    metric = Metric.find(metric_id)
+    begin
 
-    case metric.provider['provider_name']
-      when 'google'
-      when 'facebook'
-        client=Uphex::Prototype::Cynosure::Shiatsu.client(:facebook,nil,nil).authenticate(metric.provider[:access_token])
-        since= metric['updated_at'] < DateTime.parse("2014-04-01") ? DateTime.parse("2014-04-01") : metric['updated_at']
-        case metric['name']
-          when 'visits'
-            value=client.page_visits(since)
-          when 'likes'
-            value=client.page_likes(since)
-        end
-        value.value.each{|metric_day|
-          Observation.create(:metric=>metric,:index=>metric_day[:timestamp],:value=>metric_day[:payload])
-        }
-        metric['updated_at']=DateTime.now
-        metric.save!
+      metric = Metric.find(metric_id)
 
-      when 'twitter'
+      config = JSON.parse(File.read(File.expand_path("../../../config/auth_config.json", __FILE__)))
+
+      since= metric['updated_at'] < DateTime.parse("2014-04-01") ? DateTime.parse("2014-04-01") : metric['updated_at']
+
+      case metric.provider['provider_name']
+        when 'google'
+          configpart=config['oauth-v2']['providers']['google']
+          client=Uphex::Prototype::Cynosure::Shiatsu.client(:google,configpart['identifier'],configpart['secret']).authenticate(metric.provider[:access_token],metric.provider[:expiration_date],metric.provider[:refresh_token])
+          client.profile=client.profiles.find{|profile| profile.id==metric.provider['profile_id']}
+          case metric['name']
+            when 'visits'
+              value=client.visits(since,DateTime.now,:day)
+            when 'visitors'
+              value=client.visitors(since,DateTime.now,:day)
+            when 'bounces'
+              value=client.bounces(since,DateTime.now,:day)
+          end
+          value.value.each{|metric_day|
+            Observation.create(:metric=>metric,:index=>metric_day[:timestamp],:value=>metric_day[:payload])
+          }
+          metric['updated_at']=DateTime.now
+          metric.save!
+
+        when 'facebook'
+          client=Uphex::Prototype::Cynosure::Shiatsu.client(:facebook,nil,nil).authenticate(metric.provider[:access_token])
+          case metric['name']
+            when 'visits'
+              value=client.page_visits(since)
+            when 'likes'
+              value=client.page_likes(since)
+          end
+          value.value.each{|metric_day|
+            Observation.create(:metric=>metric,:index=>metric_day[:timestamp],:value=>metric_day[:payload])
+          }
+          metric['updated_at']=DateTime.now
+          metric.save!
+
+        when 'twitter'
+          configpart=config['oauth-v1']['providers']['twitter']
+          client=Uphex::Prototype::Cynosure::Shiatsu.client(:twitter,configpart['consumer_key'],configpart['consumer_secret']).authenticate(metric.provider[:access_token],metric.provider[:access_token_secret])
+          case metric['name']
+            when 'followers'
+              Observation.create(:metric=>metric,:index=>DateTime.now,:value=>client.followers_count)
+          end
+          metric['updated_at']=DateTime.now
+          metric.save!
+      end
+    rescue => e
+      puts e.inspect, e.backtrace
     end
+
   end
 end
