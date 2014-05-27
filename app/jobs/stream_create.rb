1f72a0c618aa9bcb1bc55baf1e91a27a1fb1e7f6
sashee <gsashee@gmail.com>
2014-05-27 10:09:49 +0200

    Refactor stream_create

--- app/jobs/stream_create.rb
+++ app/jobs/stream_create.rb
@@ -7,16 +7,17 @@ class StreamCreate
 
     provider=Provider.find(provider_id)
 
-    case provider['provider_name']
-      when 'google'
-        metrics << Metric.new(:provider=>provider,:name=>'visits',:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
-        metrics << Metric.new(:provider=>provider,:name=>'visitors',:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
-        metrics << Metric.new(:provider=>provider,:name=>'bounces',:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
-      when 'facebook'
-        metrics << Metric.new(:provider=>provider,:name=>'visits',:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
-        metrics << Metric.new(:provider=>provider,:name=>'likes',:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
-      when 'twitter'
-        metrics << Metric.new(:provider=>provider,:name=>'followers',:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
+    metric_names = case provider['provider_name']
+       when 'google'
+         ['visits', 'visitors', 'bounces']
+       when 'facebook'
+         ['visits', 'likes']
+       when 'twitter'
+         ['followers']
+     end
+
+    metric_names.each do |name|
+      metrics << Metric.new(:provider=>provider,:name=>name,:updated_at=>DateTime.new,:analyzed_at=>DateTime.new)
     end
 
     metrics.each{|metric|
