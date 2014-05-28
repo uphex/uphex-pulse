444ca21740280a8436d78a3044f11d3fef674232
sashee <gsashee@gmail.com>
2014-05-28 21:29:16 +0200

    possible solution to an events page bug

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,8 +12,8 @@ module EventsHelper
     if whole_sparkline
       observations=event.metric.observations.all
     else
-      start_time=event[:date].beginning_of_day
-      end_time=event[:date].beginning_of_day+1.days
+      start_time=event[:date].beginning_of_day-1.days
+      end_time=event[:date].beginning_of_day+2.days
       observations=[].concat(event.metric.observations.where('index<=:start_time',{:start_time=>start_time}).take(1)).concat(event.metric.observations.where('index>:start_time and index<:end_time',{:start_time=>start_time,:end_time=>end_time}).order('index ASC')).concat(event.metric.observations.where('index>=:end_time',{:end_time=>end_time}).take(1))
     end
 
