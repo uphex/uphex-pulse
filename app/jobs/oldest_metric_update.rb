82b5a7d6b47f50f9823ec94fab52796f27d4679f
sashee <gsashee@gmail.com>
2014-06-06 11:24:10 +0200

    OldestMetricUpdate changes: - A MetricUpdate will be scheduled for each provider - If there is an error, then the metric will be scheduled last

--- app/jobs/oldest_metric_update.rb
+++ app/jobs/oldest_metric_update.rb
@@ -1,10 +1,16 @@
 class OldestMetricUpdate
   @queue = :OldestMetricUpdate
   def self.perform
-    puts 'OldestMetricUpdate provider'
-    oldestmetric=Metric.order('updated_at ASC').first
-    unless oldestmetric.nil?
-      Resque.enqueue(MetricUpdate,oldestmetric[:id])
+    begin
+      puts 'OldestMetricUpdate provider'
+      Metric.all.group_by{|metric| metric.provider[:provider_name]}.each_pair{|provider_name,metrics|
+        Resque.enqueue(MetricUpdate,metrics.sort_by{|metric|
+          return Time.at(0) if metric[:updated_at].nil? and metric[:last_error_time].nil?
+          metric[:last_error_time].nil? or (!metric[:updated_at].nil? and metric[:updated_at]>metric[:last_error_time])?metric[:updated_at]:metric[:last_error_time]
+        }.first[:id])
+      }
+    rescue => e
+      puts e.inspect, e.backtrace
     end
   end
 end
