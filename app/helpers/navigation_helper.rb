f0a23993a7cb63712ac44b660956f174681053a7
John Feminella <jxf@jxf.me>
2014-06-16 14:43:35 -0400

    Updates navigation header to show organization name

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,7 +2,7 @@ module NavigationHelper
   def organization_name_tag
     org_name = nil
     if current_user
-      org_name = current_user.organizations.first if current_user.organizations.any?
+      org_name = current_user.organizations.first.name if current_user.organizations.any?
     end
     org_name || I18n.t("navigation.title.default")
   end
