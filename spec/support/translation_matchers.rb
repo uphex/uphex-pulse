718132a367b747b6b50d33db5119da0938ea11f9
John Feminella <jxf@jxf.me>
2014-06-20 18:54:18 -0400

    Adds RSpec translation matcher for easier specs using I18n

---
+++ spec/support/translation_matchers.rb
@@ -0,0 +1,20 @@
+RSpec::Matchers.define :have_message do |expected_key|
+  require 'cgi'
+
+  def to_html_message(key)
+    CGI.escapeHTML I18n.t key
+  end
+
+  match do |actual|
+    actual.include? to_html_message expected_key
+  end
+
+  failure_message_for_should do |actual|
+    message = to_html_message expected_key
+    %|expected that #{actual} would contain message: #{message}|
+  end
+
+  description do
+    %|contains the message for the key "#{expected_key}"|
+  end
+end
