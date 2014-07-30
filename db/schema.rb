b60628fe2ab900d216246cb4fd4a64e76e630417
sashee <gsashee@gmail.com>
2014-07-30 20:35:30 +0200

    Only hide the time part if the event's date is at the start of the day

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 13) do
+ActiveRecord::Schema.define(version: 14) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,10 @@ ActiveRecord::Schema.define(version: 13) do
   end
 
   create_table "events", force: true do |t|
-    t.date    "date"
-    t.decimal "prediction_low"
-    t.decimal "prediction_high"
-    t.integer "metrics_id"
+    t.datetime "date"
+    t.decimal  "prediction_low"
+    t.decimal  "prediction_high"
+    t.integer  "metrics_id"
   end
 
   create_table "metrics", force: true do |t|
