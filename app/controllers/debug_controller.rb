1cd6ce152d6af818e6b113dd8950acce2f0742c2
sashee <gsashee@gmail.com>
2014-08-20 16:12:57 +0200

    Debug page is separated into 2 parts

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -2,26 +2,29 @@ UpHex::Pulse.controllers :debug do
 
   get '/' do
     error(403) unless is_admin?
-    @metrics = Metric.all.sort_by{|metric| metric.provider.portfolio.organization.name+'/'+metric.provider.portfolio.name+'/'+metric.provider.name+'/'+metric.name}
-    params['metric'] ||= @metrics.first.id.to_s
 
-    metric=Metric.find(params['metric'])
-    @observations=metric.observations
-    @sparkline=SparklineNormalizer.new.normalize(metric.observations)
+    if params['metric'].nil?
+      @metrics = Metric.all.sort_by{|metric| metric.provider.portfolio.organization.name+'/'+metric.provider.portfolio.name+'/'+metric.provider.name+'/'+metric.name}
+    else
+      @metric=Metric.find(params['metric'])
+      @observations=@metric.observations
+      @sparkline=SparklineNormalizer.new.normalize(@metric.observations)
 
-    full_data=@sparkline.map{|sline|
-      {:date=>sline[:index].to_date,:value=>sline[:value].round}
-    }.sort_by{|val| val[:date]}
+      full_data=@sparkline.map{|sline|
+        {:date=>sline[:index].to_date,:value=>sline[:value].round}
+      }.sort_by{|val| val[:date]}
 
-    ts = UpHex::Prediction::TimeSeries.new(full_data, :days => 1)
-    range = 0..1
-    results = UpHex::Prediction::ExponentialMovingAverageStrategy.new(ts).comparison_forecast(1, :range => range, :confidence => 0.99)
+      ts = UpHex::Prediction::TimeSeries.new(full_data, :days => 1)
+      range = 0..1
+      results = UpHex::Prediction::ExponentialMovingAverageStrategy.new(ts).comparison_forecast(1, :range => range, :confidence => 0.99)
 
-    @bands=results.map{|result|
-      {:low=>[result[:low].floor,0].max,:high=>result[:high].ceil,:date=>result[:date]}
-    }
+      @bands=results.map{|result|
+        {:low=>[result[:low].floor,0].max,:high=>result[:high].ceil,:date=>result[:date]}
+      }
 
-    @events=metric.events
+      @events=@metric.events
+    end
+
 
     render 'debug/streams'
   end
