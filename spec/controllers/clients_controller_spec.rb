393bfcdf53760c80d69e71421b7f214af970c099
sashee <gsashee@gmail.com>
2014-08-29 17:04:23 +0200

    Formats all metric name when displaying

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -27,4 +27,43 @@ describe 'ClientsController' do
       expect(last_response.body).not_to include 'Fetching data'
     end
   end
+
+  it 'should humanize all metric name before display' do
+    create_sample_user
+    create_sample_portfolio
+    create_sample_metric
+
+    ad_click_metric=Metric.create({
+                      :provider => Provider.first,
+                      :name => 'adClicks',
+                      :updated_at => DateTime.new,
+                      :analyzed_at => DateTime.new
+                  })
+
+    get '/clients/'+Portfolio.all.first.id.to_s
+
+    expect(last_response.body).not_to include 'adClicks'
+    expect(last_response.body).to include 'Ad clicks'
+
+    Observation.create(:index=>Time.utc(2014,2,27),:value=>2,:metric=>ad_click_metric).save!
+    Observation.create(:index=>Time.utc(2014,2,28),:value=>2,:metric=>ad_click_metric).save!
+    event1=Event.create(:date=>Time.utc(2014,2,27),:prediction_low=>0,:prediction_high=>1,:metric=>ad_click_metric)
+    event1.save!
+
+    get '/clients/'+Portfolio.all.first.id.to_s
+
+    expect(last_response.body).not_to include 'adClicks'
+    expect(last_response.body).to include 'Ad clicks'
+
+    get '/users/me/dashboard'
+
+    expect(last_response.body).not_to include 'adClicks'
+    expect(last_response.body).to include 'Ad clicks'
+
+    get '/events?portfolioid='+Portfolio.all.first.id.to_s
+
+    expect(last_response.body).not_to include 'adClicks'
+    expect(last_response.body).to include 'Ad clicks'
+
+  end
 end
