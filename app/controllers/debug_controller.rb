888cda4a658b240854bc4a3b2556c132a0c1c7fd
sashee <gsashee@gmail.com>
2014-08-29 16:14:31 +0200

    Adds an event debug page

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -16,4 +16,14 @@ UpHex::Pulse.controllers :debug do
 
     render 'debug/streams'
   end
+
+  get '/events' do
+    error(403) unless is_admin?
+
+    @anomalies=Metric.all.flat_map{|metric|
+      DebugHelper.new(metric.id).anomalies
+    }.compact.group_by{|anomaly| anomaly[:date]}.to_a.sort_by{|a| a}.reverse
+
+    render 'debug/events'
+  end
 end
--- app/controllers/debug_helper.rb
+++ app/controllers/debug_helper.rb
@@ -4,32 +4,44 @@ class DebugHelper
   end
 
   def metric
-    Metric.find(@metric_id)
+    @metric ||= Metric.find(@metric_id)
   end
-
   def observations
-    metric.observations
+    @observations ||= metric.observations
   end
 
   def sparkline
-    SparklineNormalizer.new.normalize(observations)
+    @sparkline ||= SparklineNormalizer.new.normalize(observations)
   end
 
   def bands
-    full_data=sparkline.map{|sline|
-      {:date=>sline[:index].to_date,:value=>sline[:value].round}
-    }.sort_by{|val| val[:date]}
+    @bands ||= begin
+      full_data=sparkline.map{|sline|
+        {:date=>sline[:index].to_date,:value=>sline[:value].round}
+      }.sort_by{|val| val[:date]}
 
-    ts = UpHex::Prediction::TimeSeries.new(full_data, :days => 1)
-    range = 0..1
-    results = UpHex::Prediction::ExponentialMovingAverageStrategy.new(ts).comparison_forecast(1, :range => range, :confidence => 0.99)
+      ts = UpHex::Prediction::TimeSeries.new(full_data, :days => 1)
+      range = 0..1
+      results = UpHex::Prediction::ExponentialMovingAverageStrategy.new(ts).comparison_forecast(1, :range => range, :confidence => 0.99)
 
-    results.map{|result|
-      {:low=>[result[:low].floor,0].max,:high=>result[:high].ceil,:date=>result[:date]}
-    }
+      results.map{|result|
+        {:low=>[result[:low].floor,0].max,:high=>result[:high].ceil,:date=>result[:date]}
+      }
+    end
   end
 
   def events
-    metric.events
+    @events ||= metric.events
+  end
+
+  def anomalies
+    @anomalies ||= begin
+      sparkline.map{|point|
+        matching_band=bands.find{|band| band[:date]==point[:index]}
+        matching_event=events.find{|event| event[:date]==point[:index]}
+        crossing=(!matching_band.nil? and (matching_band[:high]<point[:value] or matching_band[:low]>point[:value]))
+        {:metric=>metric,:date=>point[:index],:crossing=>crossing,:event=>!matching_event.nil?} unless !crossing and matching_event.nil?
+      }.compact
+    end
   end
 end
