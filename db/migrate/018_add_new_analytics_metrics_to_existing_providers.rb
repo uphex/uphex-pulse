72bea85b9451a40cffcc84121ea9645d72c4ae1a
sashee <gsashee@gmail.com>
2014-08-29 13:26:50 +0200

    Fixes test and rubocop offense

--- db/migrate/018_add_new_analytics_metrics_to_existing_providers.rb
+++ db/migrate/018_add_new_analytics_metrics_to_existing_providers.rb
@@ -3,7 +3,7 @@ class AddNewAnalyticsMetricsToExistingProviders < ActiveRecord::Migration
     Provider.all.select {
         |provider| provider['provider_name']=='google' and provider.metrics.none? {
           |metric| metric['name']=='impressions'
-      }
+        }
     }.each { |provider|
       %w(impressions adClicks organicSearches).each do |name|
         Metric.new(:provider => provider, :name => name, :updated_at => DateTime.new, :analyzed_at => DateTime.new).save!
