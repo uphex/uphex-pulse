eedbdf1740e975b25e4cb40259e7bbfc3f6a3bce
sashee <gsashee@gmail.com>
2014-10-01 12:06:03 +0200

    Refactors the proposed sparkline algorithm code and spec

--- app/controllers/sparkline_normalizer_proposed.rb
+++ app/controllers/sparkline_normalizer_proposed.rb
@@ -2,42 +2,62 @@ class SparklineNormalizerProposed
   def normalize(sparkline)
     return [] if sparkline.empty?
     sparkline=sparkline.sort_by{|s| s[:index]}
+
+    ranges(sparkline).map{|date|
+      {:index=>date,:value=>interval_value(date,sparkline)}
+    }
+  end
+
+  def last_observation(sparkline,date)
+    sparkline.select{|observation| observation[:index]<date}.sort_by{|s| s[:index]}.last
+  end
+
+  def next_observation(sparkline,date)
+    sparkline.select{|observation| observation[:index]>=date}.sort_by{|s| s[:index]}.first
+  end
+
+  def ranges(sparkline)
     minmax=sparkline.map{|val| val[:index].to_datetime}.minmax
     interval_start=minmax[0].beginning_of_day
     if minmax[0]>minmax[0].beginning_of_day
       interval_start=minmax[0].beginning_of_day+1.days
     end
     interval_end=minmax[1].beginning_of_day
-    range=interval_start..interval_end-1.days
-    range.map{|date|
-      observations_inside=sparkline.select{|observation| observation[:index]>=date and observation[:index]<date+1}
-      if !observations_inside.empty?
-        sparkline_ranges=[]
-        observations_inside.each_with_index{|observation,idx|
-          if idx==0
-            sparkline_ranges.push({:from=>date,:to=>observation[:index],:value=>observation[:value]})
-          else
-            sparkline_ranges.push({:from=>observations_inside[idx-1][:index],:to=>observation[:index],:value=>(observations_inside[idx-1][:value]+observation[:value])/2})
-          end
-        }
-        sparkline_ranges.push({:from=>observations_inside.last[:index],:to=>date+1,:value=>observations_inside.last[:value]})
-        ranges_inside=sparkline_ranges.reject{|val| val[:from]<date && val[:to]<date || val[:from]>date+1 && val[:to]>date+1}.map{|val| {:from=>val[:from]<date ? date : val[:from],:to=>val[:to]>date+1 ? date+1 : val[:to],:value=>val[:value]}}
-        seconds_per_day=(date+1).to_time.to_i-date.to_time.to_i
-        sum=ranges_inside.map{|val|
-          interval_seconds=(val[:to].to_time.to_i-val[:from].to_time.to_i)
-          (interval_seconds.to_f)/seconds_per_day*val[:value]
-        }.reduce(:+)
-        {:index=>date,:value=>sum}
-      else
-        last_observation=sparkline.select{|observation| observation[:index]<date}.sort_by{|s| s[:index]}.last
-        next_observation=sparkline.select{|observation| observation[:index]>=date+1}.sort_by{|s| s[:index]}.first
-        last_observation_distance=date.to_time.to_i-last_observation[:index].to_time.to_i
-        next_observation_distance=next_observation[:index].to_time.to_i-(date+1).to_time.to_i
+    interval_start..interval_end-1.days
+  end
 
-        sum=(next_observation[:value]-last_observation[:value])*(last_observation_distance/(last_observation_distance.to_f+next_observation_distance.to_f))+last_observation[:value]
-        {:index=>date,:value=>sum}
-      end
-    }
+  def interval_value(date,sparkline)
 
+    def value_with_observations(date,observations_inside)
+      sparkline_ranges=[]
+      observations_inside.each_with_index{|observation,idx|
+        if idx==0
+          sparkline_ranges.push({:from=>date,:to=>observation[:index],:value=>observation[:value]})
+        else
+          sparkline_ranges.push({:from=>observations_inside[idx-1][:index],:to=>observation[:index],:value=>(observations_inside[idx-1][:value]+observation[:value])/2})
+        end
+      }
+      sparkline_ranges.push({:from=>observations_inside.last[:index],:to=>date+1,:value=>observations_inside.last[:value]})
+      ranges_inside=sparkline_ranges.reject{|val| val[:from]<date && val[:to]<date || val[:from]>date+1 && val[:to]>date+1}.map{|val| {:from=>val[:from]<date ? date : val[:from],:to=>val[:to]>date+1 ? date+1 : val[:to],:value=>val[:value]}}
+      seconds_per_day=(date+1).to_time.to_i-date.to_time.to_i
+      ranges_inside.map{|val|
+        interval_seconds=(val[:to].to_time.to_i-val[:from].to_time.to_i)
+        (interval_seconds.to_f)/seconds_per_day*val[:value]
+      }.reduce(:+)
+    end
+
+    def value_without_observations(date,last_observation,next_observation)
+      last_observation_distance=date.to_time.to_i-last_observation[:index].to_time.to_i
+      next_observation_distance=next_observation[:index].to_time.to_i-(date+1).to_time.to_i
+
+      (next_observation[:value]-last_observation[:value])*(last_observation_distance/(last_observation_distance.to_f+next_observation_distance.to_f))+last_observation[:value]
+    end
+
+    observations_inside=sparkline.select{|observation| observation[:index]>=date and observation[:index]<date+1}
+    if observations_inside.empty?
+      value_without_observations(date,last_observation(sparkline,date),next_observation(sparkline,date+1))
+    else
+      value_with_observations(date,observations_inside)
+    end
   end
 end
--- spec/controllers/sparkline_normalizer_proposed_spec.rb
+++ spec/controllers/sparkline_normalizer_proposed_spec.rb
@@ -2,77 +2,79 @@ require File.expand_path(File.dirname(__FILE__) + "/../../app/controllers/sparkl
 require 'spec/environment_spec_helper'
 
 describe 'SparklineNormalizerProposed' do
-  describe 'single day' do
-    it 'should work for some data points in a day', :broken => true do
-      expect(SparklineNormalizerProposed.new.normalize([
-                                                   {
-                                                       :index=>DateTime.new(2014,5,4,0,0),
-                                                       :value=>100
-                                                   },
-                                                   {
-                                                       :index=>DateTime.new(2014,5,4,12,0),
-                                                       :value=>200
-                                                   },
-                                                   {
-                                                       :index=>DateTime.new(2014,5,5,0,0),
-                                                       :value=>60
-                                                   }
-                                               ])).to match_array(
-                                                          [{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>175}]
-                                                      )
-    end
 
-    it 'should return the observation if there is only one for a day' do
-      expect(SparklineNormalizerProposed.new.normalize([
-                                                           {
-                                                               :index=>DateTime.new(2014,5,4,0,0),
-                                                               :value=>100
-                                                           },
-                                                           {
-                                                               :index=>DateTime.new(2014,5,5,0,0),
-                                                               :value=>60
-                                                           }
-                                                       ])).to match_array(
-                                                                  [{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>100}]
-                                                              )
-    end
+  before do
+    @normalizer=SparklineNormalizerProposed.new
+  end
+  it 'should work for some data points in a day', :broken => true do
+    observations=[
+        {
+            :index=>DateTime.new(2014,5,4,0,0),
+            :value=>100
+        },
+        {
+            :index=>DateTime.new(2014,5,4,12,0),
+            :value=>200
+        },
+        {
+            :index=>DateTime.new(2014,5,5,0,0),
+            :value=>60
+        }
+    ]
+    expected=[{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>175}]
+    expect(@normalizer.normalize(observations)).to match_array(expected)
+  end
 
-    it 'should step when there is a gap' do
-      expect(SparklineNormalizerProposed.new.normalize([
-                                                           {
-                                                               :index=>DateTime.new(2014,5,4,0,0),
-                                                               :value=>100
-                                                           },
-                                                           {
-                                                               :index=>DateTime.new(2014,5,7,0,0),
-                                                               :value=>200
-                                                           }
-                                                       ])).to match_array(
-                                                                  [{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>100},
-                                                                   {:index=>DateTime.new(2014,5,5,0,0).to_date,:value=>150},
-                                                                  {:index=>DateTime.new(2014,5,6,0,0).to_date,:value=>200}]
-                                                              )
-    end
+  it 'should return the observation if there is only one for a day' do
+    observations=[
+        {
+            :index=>DateTime.new(2014,5,4,0,0),
+            :value=>100
+        },
+        {
+            :index=>DateTime.new(2014,5,5,0,0),
+            :value=>60
+        }
+    ]
+    expected=[{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>100}]
+    expect(@normalizer.normalize(observations)).to match_array(expected)
+  end
 
-    it 'should step down when there is a gap downwards' do
-      expect(SparklineNormalizerProposed.new.normalize([
-                                                           {
-                                                               :index=>DateTime.new(2014,5,4,0,0),
-                                                               :value=>200
-                                                           },
-                                                           {
-                                                               :index=>DateTime.new(2014,5,7,0,0),
-                                                               :value=>100
-                                                           }
-                                                       ])).to match_array(
-                                                                  [{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>200},
-                                                                   {:index=>DateTime.new(2014,5,5,0,0).to_date,:value=>150},
-                                                                   {:index=>DateTime.new(2014,5,6,0,0).to_date,:value=>100}]
-                                                              )
-    end
+  it 'should step when there is a gap' do
+    observations=[
+        {
+            :index=>DateTime.new(2014,5,4,0,0),
+            :value=>100
+        },
+        {
+            :index=>DateTime.new(2014,5,7,0,0),
+            :value=>200
+        }
+    ]
+    expected=[{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>100},
+              {:index=>DateTime.new(2014,5,5,0,0).to_date,:value=>150},
+              {:index=>DateTime.new(2014,5,6,0,0).to_date,:value=>200}]
+    expect(@normalizer.normalize(observations)).to match_array(expected)
+  end
 
-    it 'should return an empty array if called with an empty array' do
-      expect(SparklineNormalizerProposed.new.normalize([])).to match_array([])
-    end
+  it 'should step down when there is a gap downwards' do
+    observations=[
+        {
+            :index=>DateTime.new(2014,5,4,0,0),
+            :value=>200
+        },
+        {
+            :index=>DateTime.new(2014,5,7,0,0),
+            :value=>100
+        }
+    ]
+    expected=[{:index=>DateTime.new(2014,5,4,0,0).to_date,:value=>200},
+              {:index=>DateTime.new(2014,5,5,0,0).to_date,:value=>150},
+              {:index=>DateTime.new(2014,5,6,0,0).to_date,:value=>100}]
+    expect(@normalizer.normalize(observations)).to match_array(expected)
+  end
+
+  it 'should return an empty array if called with an empty array' do
+    expect(@normalizer.normalize([])).to match_array([])
   end
 end
