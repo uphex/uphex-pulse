ec2221401bc6e6099a0ca89f80f27dcc40ed1f3e
sashee <gsashee@gmail.com>
2014-10-01 20:10:30 +0200

    Refactors the proposed sparkline algorithm

--- app/controllers/sparkline_normalizer_proposed.rb
+++ app/controllers/sparkline_normalizer_proposed.rb
@@ -26,6 +26,18 @@ class SparklineNormalizerProposed
     interval_start..interval_end-1.days
   end
 
+  def range_outside?(range,from,to)
+    range[:from]<from && range[:to]<from || range[:from]>to+1 && range[:to]>to+1
+  end
+
+  def trim_range(range,from,to)
+    {:from=>range[:from]<from ? from : range[:from],:to=>range[:to]>to ? to : range[:to],:value=>range[:value]}
+  end
+
+  def average_value(observation1,observation2)
+    (observation1[:value]+observation2[:value])/2
+  end
+
   def interval_value(date,sparkline)
 
     def value_with_observations(date,observations_inside)
@@ -34,11 +46,13 @@ class SparklineNormalizerProposed
         if idx==0
           sparkline_ranges.push({:from=>date,:to=>observation[:index],:value=>observation[:value]})
         else
-          sparkline_ranges.push({:from=>observations_inside[idx-1][:index],:to=>observation[:index],:value=>(observations_inside[idx-1][:value]+observation[:value])/2})
+          sparkline_ranges.push({:from=>observations_inside[idx-1][:index],:to=>observation[:index],:value=>average_value(observations_inside[idx-1],observation)})
         end
       }
       sparkline_ranges.push({:from=>observations_inside.last[:index],:to=>date+1,:value=>observations_inside.last[:value]})
-      ranges_inside=sparkline_ranges.reject{|val| val[:from]<date && val[:to]<date || val[:from]>date+1 && val[:to]>date+1}.map{|val| {:from=>val[:from]<date ? date : val[:from],:to=>val[:to]>date+1 ? date+1 : val[:to],:value=>val[:value]}}
+      ranges_inside=sparkline_ranges
+        .reject { |range| range_outside?(range, date, date+1) }
+        .map { |range| trim_range(range, date, date+1) }
       seconds_per_day=(date+1).to_time.to_i-date.to_time.to_i
       ranges_inside.map{|val|
         interval_seconds=(val[:to].to_time.to_i-val[:from].to_time.to_i)
